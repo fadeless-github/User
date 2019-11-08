219d7f72257db21d68dc253a7a5ab24d33f59535
fadeless-github <54878230+fadeless-github@users.noreply.github.com>
2019-11-08 13:59:23 +0800

    Update Register.java

--- Register/src/com/apple/Register/Register.java
+++ Register/src/com/apple/Register/Register.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Register {
     public static void main(String[] args){
         JFrame frame = new JFrame("Register Example");
-        frame.setSize(350, 200);
+        frame.setSize(360, 240);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
@@ -19,30 +19,40 @@ public class Register {
 
         panel.setLayout(null);
 
-        // 创建 JLabel
-        JLabel userLabel = new JLabel("User:");
-
-        userLabel.setBounds(10,20,80,25);
-        panel.add(userLabel);
-
-
         JTextField userText = new JTextField(20);
         userText.setBounds(100,20,165,25);
         panel.add(userText);
 
+        JLabel userLabel = new JLabel("User:");
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
 
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(10,50,80,25);
-        panel.add(passwordLabel);
+
+
+        JTextField mailboxText = new JTextField(20);
+        mailboxText.setBounds(100,50,165,25);
+        panel.add(mailboxText);
+
+        JLabel mailboxLabel = new JLabel("Mailbox:");
+        mailboxLabel.setBounds(10,50,80,25);
+        panel.add(mailboxLabel);
+
 
 
         JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(100,50,165,25);
+        passwordText.setBounds(100,80,165,25);
         panel.add(passwordText);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(10,80,80,25);
+        panel.add(passwordLabel);
+
+
+
 
 
         JButton RegisterButton = new JButton("Register");
-        RegisterButton.setBounds(10, 80, 80, 25);
+        RegisterButton.setBounds(10, 120, 100, 25);
         panel.add(RegisterButton);
     }
 }
